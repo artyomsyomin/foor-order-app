ede1bb6498f4b5d1d91a5995bde9fb78deb96e91
Artyom <seminartem98@mail.ru>
2022-09-04 18:04:53 +0700

    added basic components and styles

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Food Order App</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A Table of food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
